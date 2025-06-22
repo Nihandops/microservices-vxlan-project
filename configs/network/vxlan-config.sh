381672c7540a5e2398e89faf7df586a73663c766
Nihan <128370614+Nihandops@users.noreply.github.com>
2025-06-22 10:58:00 +0600

    Update vxlan-config.sh

--- configs/network/vxlan-config.sh
+++ configs/network/vxlan-config.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
-# VXLAN simulation mode: skipping ip link setup due to lab limitations
+# VXLAN Configuration for Multi-Datacenter Network Setup
 
-echo "VXLAN setup skipped (running in restricted/lab environment)"
+# Define VXLAN interfaces for DC1, DC2, and DC3
+
+# DC1 Network VXLAN Setup
+echo "Setting up VXLAN for DC1..."
+ip link add vxlan200 type vxlan id 200 dev eth0 dstport 4789
+ip addr add 10.200.1.1/16 dev vxlan200
+ip link set vxlan200 up
+
+# DC2 Network VXLAN Setup
+echo "Setting up VXLAN for DC2..."
+ip link add vxlan300 type vxlan id 300 dev eth0 dstport 4789
+ip addr add 10.300.1.1/16 dev vxlan300
+ip link set vxlan300 up
+
+# DC3 Network VXLAN Setup
+echo "Setting up VXLAN for DC3..."
+ip link add vxlan400 type vxlan id 400 dev eth0 dstport 4789
+ip addr add 10.400.1.1/16 dev vxlan400
+ip link set vxlan400 up
+
+# Display VXLAN interfaces and routes
+echo "Displaying VXLAN interfaces and routes..."
+ip a show vxlan200
+ip a show vxlan300
+ip a show vxlan400
+ip route show
